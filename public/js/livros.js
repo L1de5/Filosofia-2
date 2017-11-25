1e149af9fcb1d6e988b268d1faa5f340cd8847fd
Rodrigo Martins <contawarface750@gmail.com>
2017-11-25 20:19:27 -0200

    set on database

--- public/js/livros.js
+++ public/js/livros.js
@@ -1,5 +1,6 @@
 var usuario;
 
+
 firebase.auth().onAuthStateChanged(function (user) {
     (user)?usuario = user:window.location.replace('../index.html');
 });
@@ -14,10 +15,6 @@ var adicionaAosFavoritos = function () {
         }
     });
 }
-
-
-
-
 
 $('#deslogar')[0].addEventListener('click', function (){
     firebase.auth().signOut().then(()=> window.location.replace('../index.html')).catch((error)=> alert('Erro ao deslogar'));
