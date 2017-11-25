0c87a832539a950d8f2e8b28efe92f2cdad1440a
L1de5 <jp-20011973@hotmail.com>
2017-11-25 21:36:55 -0200

    Troca de senha

--- public/js/livros.js
+++ public/js/livros.js
@@ -5,8 +5,6 @@ firebase.auth().onAuthStateChanged(function (user) {
     (user)?usuario = user:window.location.replace('../index.html');
 });
 
-<<<<<<< HEAD
-=======
 var adicionaAosFavoritos = function () {
     firebase.database().ref('user/' + usuario.email.split('@')[0]).set({
         favoritos: {
@@ -17,7 +15,40 @@ var adicionaAosFavoritos = function () {
         }
     });
 }
->>>>>>> 1e149af9fcb1d6e988b268d1faa5f340cd8847fd
+$('#trocarSenha')[0].addEventListener('click', function () {
+    var usuario = firebase.auth().currentUser;
+    var senha = $('#senha')[0].value;
+    var confSenha = $('#confSenha')[0].value;
+    if(senha === confSenha){
+        usuario.updatePassaword(senha);
+    }else{
+        buildErrorMessage("Senhas não batem");
+    }
+});
+var buildErrorMessage = function (mensagem) {
+    var div = document.createElement('div');
+    div.setAttribute('class', 'alert alert-danger alert-dismissible fade show');
+    div.setAttribute('role', 'alert');
+
+    var teste = document.createElement('span');
+    teste.textContent = mensagem;
+
+    var btn = document.createElement('button');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('class', 'close');
+    btn.setAttribute('data-dismiss', 'alert');
+    btn.setAttribute('aria-label', 'close');
+
+    var span = document.createElement('span');
+    span.setAttribute('aria-hidden', 'true');
+    span.innerHTML = '&times;';
+
+    btn.appendChild(span);
+    div.appendChild(teste);
+    div.appendChild(btn);
+
+    return $('#error')[0].appendChild(div);
+}
 
 $('#deslogar')[0].addEventListener('click', function (){
     firebase.auth().signOut().then(()=> window.location.replace('../index.html')).catch((error)=> alert('Erro ao deslogar'));
